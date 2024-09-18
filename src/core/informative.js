e6c0ad3caadeaeeba4df3253769c7f3bf4057b54
Nicola Mascotto <nicolamascotto@gmail.com>
2024-09-19 01:04:26 +0200

    Update informative.js

--- src/core/informative.js
+++ src/core/informative.js
@@ -10,6 +10,9 @@ const localizationStrings = {
   en: {
     informative: "This section is non-normative.",
   },
+  it: {
+    informative: "Questa sezione è non normativa.",
+  },
   nl: {
     informative: "Dit onderdeel is niet normatief.",
   },
