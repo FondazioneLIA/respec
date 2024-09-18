e5adcf6e8c476b9af5b875e9e84625fe89c3f175
Nicola Mascotto <nicolamascotto@gmail.com>
2024-09-19 00:40:40 +0200

    Update conformance.js

--- src/aom/conformance.js
+++ src/aom/conformance.js
@@ -33,6 +33,7 @@ const localizationStrings = {
       "è normativo.",
     keywordInterpretation(keywords, plural) {
       return html`<p>
+        ${plural ? "Le" : "La"}
         parol${plural ? "e" : "a"} chiave ${keywords} in questo documento
         ${plural ? "sono" : "è"} da interpretare come descritto nel
         <a href="https://datatracker.ietf.org/doc/html/bcp14">BCP 14</a>
