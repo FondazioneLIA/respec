e149be67ca91b28763a10c3437213acd3c614674
Nicola Mascotto <nicolamascotto@gmail.com>
2024-09-19 03:16:44 +0200

    Update sotd.js

--- src/w3c/templates/sotd.js
+++ src/w3c/templates/sotd.js
@@ -15,8 +15,8 @@ const localizationStrings = {
     sotd: "Stato di questo documento",
     status_at_publication: html`Questa sezione descrive lo stato di questo
       documento al momento della sua pubblicazione. Un elenco delle attuali
-      pubblicazioni del W3C e l'ultima revisione di questo rapporto tecnico
-      si trovano nell'<a href="https://www.w3.org/TR/">indice dei rapporti
+      pubblicazioni del W3C e l'ultima revisione di questo rapporto tecnico si
+      trovano nell'<a href="https://www.w3.org/TR/">indice dei rapporti
       tecnici del W3C</a> su https://www.w3.org/TR/.`,
   },
   ko: {
