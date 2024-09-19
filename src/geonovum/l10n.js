7cb4f93736aa865916d14fb2253683ce8c915d55
Nicola Mascotto <nicolamascotto@gmail.com>
2024-09-19 02:34:48 +0200

    Update l10n.js

--- src/geonovum/l10n.js
+++ src/geonovum/l10n.js
@@ -21,8 +21,8 @@ const additions = {
       sostituire questo documento. Un elenco delle pubblicazioni attuali di
       Geonovum e l'ultima revisione di questo documento sono disponibili su
       <a href="https://www.geonovum.nl/geo-standaarden/alle-standaarden"
-        >https://www.geonovum.nl/geo-standaarden/alle-standaarden</a>
-      (in olandese).`,
+        >https://www.geonovum.nl/geo-standaarden/alle-standaarden</a
+      >(in olandese).`,
   },
   nl: {
     status_at_publication: html`Deze paragraaf beschrijft de status van dit
